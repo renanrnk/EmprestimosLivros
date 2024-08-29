bf938b2e3b170e734a7b3541572e6566f0027a68
Renan Nogueira <renan.dev012@gmail.com>
2024-08-29 12:31:08 -0300

    Inicio do projeto

---
+++ EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -0,0 +1,150 @@
+﻿using ClosedXML.Excel;
+using EmprestimoLivros.Data;
+using EmprestimoLivros.Models;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+
+        readonly private ApplicationDbContext _db;
+
+        public EmprestimoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimoModel> emprestimos = _db.Emprestimos;
+
+            return View(emprestimos);
+        }
+
+        public IActionResult Exportar()
+        {
+            var dados = GetDados();
+            using (XLWorkbook workbook = new XLWorkbook())
+            {
+                workbook.AddWorksheet(dados, "Dados Empréstimos");
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    workbook.SaveAs(ms);
+                    return File(ms.ToArray(), "application/vnd.openxlm-formats-officedocument.sprendsheetml.sheet", "Emprestimo.xls");
+                }
+            }                
+        }
+
+        private DataTable GetDados()
+        {
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("Recebedor", typeof(string));
+            dataTable.Columns.Add("Fornecedor", typeof(string));
+            dataTable.Columns.Add("Livro Emprestado", typeof(string));
+            dataTable.Columns.Add("Data do Empréstimo", typeof(DateTime));
+
+            var dados = _db.Emprestimos.ToList();
+            if(dados.Count > 0)
+            {
+                dados.ForEach(emprestimo =>
+                dataTable.Rows.Add(emprestimo.Recebedor, emprestimo.Fornecedor, emprestimo.LivroEmprestado, emprestimo.DataEmprestimo));
+            }
+            return dataTable;
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            EmprestimoModel emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            return View(emprestimo);
+        }
+        [HttpGet]
+        public IActionResult Excluir(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            EmprestimoModel emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+            return View(emprestimo);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimoModel emprestimos)
+        {
+            if (ModelState.IsValid)
+            {
+                emprestimos.DataEmprestimo = DateTime.Now;
+
+                _db.Emprestimos.Add(emprestimos);
+                _db.SaveChanges();
+                TempData["MensagemSucesso"] = "Cadastro realizado com sucesso!";
+                return RedirectToAction("Index");
+            }         
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Editar(EmprestimoModel emprestimo)
+        {
+            if (ModelState.IsValid)
+            {
+                var emprestimoDB = _db.Emprestimos.Find(emprestimo.Id);
+
+                emprestimoDB.Fornecedor = emprestimo.Fornecedor;
+                emprestimoDB.Recebedor = emprestimo.Recebedor;
+                emprestimoDB.LivroEmprestado = emprestimo.LivroEmprestado;
+
+                _db.Emprestimos.Update(emprestimoDB);
+                _db.SaveChanges();
+
+                TempData["MensagemSucesso"] = "Edição realizado com sucesso!";
+                return RedirectToAction("Index");
+            }
+
+            TempData["MensagemErro"] = "Algum erro ocorreu ao realizar a ediçao";
+
+            return View(emprestimo);
+        }
+        [HttpPost]
+        public IActionResult Excluir(EmprestimoModel emprestimo)
+        {
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            _db.Emprestimos.Remove(emprestimo);
+            _db.SaveChanges();
+            TempData["MensagemSucesso"] = "Remoçao realizado com sucesso!";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ EmprestimoLivros/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    $('#Emprestimos').DataTable({
+        language: {
+            "decimal": "",
+            "emptyTable": "No data available in table",
+            "info": "Mostrando _START_ de _END_ no total de _TOTAL_ entradas",
+            "infoEmpty": "Showing 0 to 0 of 0 entries",
+            "infoFiltered": "(filtrado de _MAX_ no total)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ entradas",
+            "loadingRecords": "Loading...",
+            "processing": "",
+            "search": "Procurar:",
+            "zeroRecords": "No matching records found",
+            "paginate": {
+                "first": "Primeiro",
+                "last": "Último",
+                "next": "Próximo",
+                "previous": "Anterior"
+            },
+            "aria": {
+                "orderable": "Order by this column",
+                "orderableReverse": "Reverse order this column"
+            }
+        }
+    });
+
+    setTimeout(function () {
+        $(".alert").fadeOut("slow", function () {
+            $(this).alert('close')
+        })
+    }, 3500)
+});
